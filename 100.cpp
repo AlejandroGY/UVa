8c0515c9d27d0ff99af0caf40b03282f493d56c7
AlejandroGY <alejandro.gy96@gmail.com>
2017-10-15 13:10:49 -0500

    UVa

---
+++ 100.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <algorithm>
+#define ull unsigned long long int
+using namespace std;
+ 
+int memo[1000000];
+
+int f(ull n) {
+	if(n == 1) return 1;
+	int r = 0;
+	while(n >= 1000000) {
+		n = (n%2 == 0 ? n/2 : 3*n+1);
+		r++;
+	}
+	if(memo[n] == 0) memo[n] = (n%2 == 0 ? f(n/2) : f(3*n+1)) + 1;
+	return memo[n] + r;
+}
+ 
+int main() {
+	int n, m;
+	while (scanf("%d%d", &n, &m) == 2) {
+		int res = 0;
+		printf("%d %d ", n, m);
+		if (n > m) {
+			m = m + n;
+			n = m - n;
+			m = m - n;
+		}
+		for(int i = n; i <= m; ++i) {
+			res = max(res, f(i));
+		}
+		printf("%d\n", res);
+	}
+}
