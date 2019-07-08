e47150842f009b5b4920426cc0ac423c61877e2d
Alejandro González Yáñez <alejandro.gy96@gmail.com>
2019-07-08 00:40:27 -0500

    Create 11991.cpp

---
+++ 11991.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using ll = long long;
+constexpr ll MAX = 100010;
+
+int main( ) {
+   std::ios_base::sync_with_stdio(0);
+   std::cin.tie(0);
+   std::cout.tie(0);
+
+   int n, m;
+   while (std::cin >> n >> m) {
+      std::pair<int, int> arr[n];
+      for (int i = 1; i <= n; ++i) {
+         std::cin >> arr[i - 1].first;
+         arr[i - 1].second = i;
+      }
+      std::sort(arr, arr + n);
+
+      for (int i = 0; i < m; ++i) {
+         int k, v;
+         std::cin >> k >> v;
+         auto it = std::lower_bound(arr, arr + n, std::make_pair(v, 0)) - arr;
+         if (it < n && arr[it].first == v && (it + k - 1) < n) {
+            if (arr[it + k - 1].first == v) {
+               std::cout << arr[it + k - 1].second << "\n";
+            } else {
+               std::cout << "0\n";   
+            }
+         } else {
+            std::cout << "0\n";
+         }
+      }
+   }
+}
