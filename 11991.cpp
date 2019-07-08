c89a70f220e674a2387ad77a36450b2cb4a4f324
Alejandro González Yáñez <alejandro.gy96@gmail.com>
2019-07-08 00:41:48 -0500

    Update 11991.cpp

--- 11991.cpp
+++ 11991.cpp
@@ -21,12 +21,8 @@ int main( ) {
          int k, v;
          std::cin >> k >> v;
          auto it = std::lower_bound(arr, arr + n, std::make_pair(v, 0)) - arr;
-         if (it < n && arr[it].first == v && (it + k - 1) < n) {
-            if (arr[it + k - 1].first == v) {
-               std::cout << arr[it + k - 1].second << "\n";
-            } else {
-               std::cout << "0\n";   
-            }
+         if (it < n && arr[it].first == v && (it + k - 1) < n && arr[it + k - 1].first == v) {
+            std::cout << arr[it + k - 1].second << "\n";
          } else {
             std::cout << "0\n";
          }
